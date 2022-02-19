901e691bf049b050a663b46c59bd06dd5f0eff2e
ItsMrNatural <premkishore273@gmail.com>
2022-02-19 17:23:25 +0530

    Few theme and config changes

---
+++ pypke-docs/blog/2022-02-19-first-blog/index.md
@@ -0,0 +1,10 @@
+---
+slug: first-post
+title: First Blog Post
+authors: [natural]
+tags: [first]
+---
+
+Yo, This is first of many blogs here. Just checking if it works cause it would be bad if it doesn't right?
+
+Anyways join the support server if aren't joined already.
--- pypke-docs/docusaurus.config.js
+++ pypke-docs/docusaurus.config.js
@@ -1,118 +1,140 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+	title: "Pypke Bot",
+	tagline: "A bot for all your needs.",
+	url: "https://your-docusaurus-test-site.com",
+	baseUrl: "/",
+	onBrokenLinks: "warn",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+	organizationName: "Pypke", // Usually your GitHub org/user name.
+	projectName: "pypke-docs", // Usually your repo name.
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: "/",
+					sidebarPath: require.resolve("./sidebars.js"),
+					editUrl: "https://github.com/pypke/pypke-docs-v2",
+					showLastUpdateAuthor: true,
+					showLastUpdateTime: true,
+				},
+				blog: {
+					showReadingTime: true,
+					editUrl: "https://github.com/pypke/pypke-docs-v2",
+				},
+				theme: {
+					customCss: [
+						require.resolve("./src/css/lightTheme.css"),
+						require.resolve("./src/css/darkTheme.css"),
+						require.resolve("./src/css/extraComponents.css"),
+					],
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			colorMode: {
+				respectPrefersColorScheme: true,
+			},
+			metadata: [
+				{
+					name: "og:image",
+					content: "https://guide.disnake.dev/static/img/logo.png",
+				},
+				{
+					name: "theme-color",
+					content: "#feb402",
+				},
+				{
+					name: "twitter:card",
+					content: "summary",
+				},
+			],
+			navbar: {
+				title: "Pypke Bot",
+				logo: {
+					alt: "My Site Logo",
+					src: "img/logo.png",
+				},
+				items: [
+					{
+						type: "doc",
+						docId: "intro",
+						position: "left",
+						label: "Docs",
+					},
+					{ to: "/blog", label: "Blog", position: "left" },
+					{
+						href: "https://discord.gg/wait",
+						className: "navbar-item-discord",
+						position: "right",
+					},
+					{
+						href: "https://github.com/pypke/pypke-docs-v2",
+						className: "navbar-item-github",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					{
+						title: "Docs",
+						items: [
+							{
+								label: "Tutorial",
+								to: "/docs/intro",
+							},
+						],
+					},
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Discord",
+								href: "https://discordapp.com/invite/docusaurus",
+							},
+							{
+								label: "Twitter",
+								href: "https://twitter.com/docusaurus",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "Blog",
+								to: "/blog",
+							},
+							{
+								label: "GitHub",
+								href: "https://github.com/facebook/docusaurus",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Pypke. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
